8712d60b23b6226e194edf093a0e34f185d12569
kaua <kauadt2000@gmail.com>
2025-05-03 19:28:16 -0300

    Endpoints with database connection (CRUD)

--- src/app.js
+++ src/app.js
@@ -1,16 +1,9 @@
 import express from 'express'
+import connection from '../infra/connection.js'
 const app = express()
 
 // Indicate to express read the body with JSON 
 app.use(express.json())
-
-// mock
-const teams = [
-    { id: 1, team: "Brasil", group: 'G' },
-    { id: 2, team: "Suiça", group: 'G' },
-    { id: 3, team: "Camarões", group: 'G' },
-    { id: 4, team: "Sérvia", group: 'G' }
-]
 
 function findTeamById(id) {
     return teams.filter(team => team.id == id)
@@ -21,36 +14,67 @@ function findTeamIndex(id) {
     return teams.findIndex(team => team.id == id)
 }
 
-// Create the default route (default --> index)
-app.get('/', (req, res) => {
-    res.send('Hello, World!')
-})
+// Routes
 
 app.get('/selecoes', (req, res) => {
-    res.status(200).send(teams)
+    const sql = "SELECT * FROM teams;"
+    connection.query(sql, (error, result) => {
+        if (error) {
+            res.status(404).json({ "error": error })
+            return
+        }
+        res.status(200).json(result)
+    })
 })
 
 app.get('/selecoes/:id', (req, res) => {
-    res.json(findTeamById(req.params.id))
+    const id = req.params.id
+    const sql = "SELECT * FROM teams WHERE id=?;"
+    connection.query(sql, id, (error, result) => {
+        const row = result[0]
+        if (error) {
+            res.status(404).json({ "error": error })
+            return
+        }
+        res.status(200).json(row)
+    })
 })
 
 app.post('/selecoes', (req, res) => {
-    selecoes.push(req.body)
-    res.status(201).send('Team resgistered sucessfully!')
+    const team = req.body
+    const sql = "INSERT INTO teams SET ?;"
+    connection.query(sql, data, (error, result) => {
+        if (error) {
+            res.status(400).json({ "error": error })
+            return
+        }
+        res.status(201).json(result)
+    })
 })
 
 app.delete('/selecoes/:id', (req, res) => {
-    let index = findTeamIndex(req.params.id)
-    // Remove from position to position
-    teams.splice(index, 1)
-    res.send(`Team with ID ${req.params.id} was deleted sucessfully!`)
+    const id = req.params.id
+    const sql = "DELETE FROM teams WHERE id=?;"
+    connection.query(sql, id, (error, result) => {
+        if (error) {
+            res.status(404).json({ "error": error })
+            return
+        }
+        res.status(200).json(result)
+    })
 })
 
 app.put('/selecoes/:id', (req, res) => {
-    let index = findTeamIndex(req.params.id)
-    teams[index].team = req.body.team
-    teams[index].group = req.body.group
-    res.json(teams)
+    const team = req.body
+    const id = req.params.id
+    const sql = "UPDATE teams SET ? WHERE id = ?;"
+    connection.query(sql, [team, id], (error, result) => {
+        if (error) {
+            res.status(400).json({ "error": error })
+            return
+        }
+        res.status(200).json(result)
+    })
 })
 
 export default app
